c8dbf5a0a15eed0584fcdc7b774553ed758ef9c8
Anna Odintsova <Klio7@mail.ru>
2023-12-19 14:37:04 +0300

    feat: solution of carbon,common character,count cats,delete digit, dream-team

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,22 +6,34 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") {
+    return false;
+  } else {
+    let age = parseFloat(sampleActivity);
+    if (
+      (Number.isInteger(age) && age <= 15 && age > 0) ||
+      (Number.parseFloat(age) && age <= 15 && age > 0)
+    ) {
+      let calculatedAge =
+        Math.log(MODERN_ACTIVITY / age) / (0.693 / HALF_LIFE_PERIOD);
+      return Math.ceil(calculatedAge);
+    }
+  }
+  return false;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let newArray = [];
+  for (let i = 0; i < domains.length; i++) {
+    newArray.push(domains[i].split(".").reverse());
+  }
+
+  let set = new Set(newArray);
+  return newArray;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
